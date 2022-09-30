b7b85826326c3fc91ee47bd605645b6a8608c9b6
tommy inouye <tommyinouye@tommys-Air.lan>
2022-09-30 11:42:47 -0400

    Second commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "traveldeals6cd7e331": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "traveldeals": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Icon, Menu, Dropdown } from 'semantic-ui-react'
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className='App'>
+              <Menu fixed='top' color='teal' inverted>
+                  <Menu.Menu>
+                      <Menu.Item header href='/'><Icon name='globe'/>Travel Deals</Menu.Item>
+                  </Menu.Menu>
+                  <Menu.Menu position='right'>
+                      <Dropdown item simple text={user.username}>
+                          <Dropdown.Menu>
+                              <Dropdown.Item onClick={() => signOut()}><Icon name='power off'/>Log Out</Dropdown.Item>
+                          </Dropdown.Menu>
+                      </Dropdown>
+                  </Menu.Menu>
+              </Menu>
+
+          </div>
+      </>
+
+
+
   );
 }
 
-export default App;
+export default withAuthenticator(App);
